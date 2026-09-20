336460d5d1c0486f56125680e5cc891c43be49b6
Abubakar Yimam <abubakaryimam37@gmail.com>
2026-09-20 22:32:37 +0300

    Read connection string from env variable first
    Updated code to set connectionString from the "ConnectionStrings__DefaultConnection" environment variable, falling back to the hardcoded value if not set. This enhances flexibility and security by allowing external configuration.

--- TelegramStudentManager.DataAccess/clsDataSetthing.cs
+++ TelegramStudentManager.DataAccess/clsDataSetthing.cs
@@ -5,12 +5,13 @@ namespace TelegramStudentManager.DataAccess
     public class clsDataSetthing
     {
         public static string connectionString =
-            "Host=aws-1-eu-west-1.pooler.supabase.com;" +
-            "Port=5432;" +
-            "Database=postgres;" +
-            "Username=postgres.lucisablddsjlvlyknzk;" +
-            "Password=abukdi6187@.com;" +
-            "SSL Mode=Require;" +
-            "Trust Server Certificate=True;";
+            Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection")
+            ?? "Host=aws-1-eu-west-1.pooler.supabase.com;" +
+               "Port=5432;" +
+               "Database=postgres;" +
+               "Username=postgres.lucisablddsjlvlyknzk;" +
+               "Password=abukdi6187@.com;" +
+               "SSL Mode=Require;" +
+               "Trust Server Certificate=True;";
     }
 }
